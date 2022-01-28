8e4587d1c8d5c9fa6bb5493fdd5e79efa19f9731
gustavopirro <gustavo.pirro@gmail.com>
2022-01-28 09:44:14 -0300

    feat: implemented article, launch and event generic api views

--- api/views.py
+++ api/views.py
@@ -1,6 +1,37 @@
-from django.http import HttpResponse
+from rest_framework import generics
+from api.models import Article
+from api.models import Launch
+from api.models import Event
+from api.serializers import ArticleSerializer
+from api.serializers import LaunchSerializer
+from api.serializers import EventSerializer
 
 
-# Create your views here.
-def hello_world(request):
-    return HttpResponse("Hello world")
+class ArticleList(generics.ListCreateAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class ArticleDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Article.objects.all()
+    serializer_class = ArticleSerializer
+
+
+class LaunchList(generics.ListCreateAPIView):
+    queryset = Launch.objects.all()
+    serializer_class = LaunchSerializer
+
+
+class LaunchDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Launch.objects.all()
+    serializer_class = LaunchSerializer
+
+
+class EventList(generics.ListCreateAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
+
+
+class EventDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Event.objects.all()
+    serializer_class = EventSerializer
