b734dcd931e7c431feb3d8ca4a43cad129e1c7d4
gustavopirro <gustavo.pirro@gmail.com>
2022-01-28 09:37:51 -0300

    fix: fixed wrong import on models.py and added __str__ method

--- api/models.py
+++ api/models.py
@@ -1,23 +1,28 @@
 from django.db import models
-from django.forms import BooleanField, CharField, DateTimeField, URLField
 
 
 class Event(models.Model):
-    provider = CharField(max_length=100)
+    provider = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.provider
 
 
 class Launch(models.Model):
-    provider = CharField(max_length=100)
+    provider = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.provider
 
 
 class Article(models.Model):
-    title = CharField(max_length=150)
-    url = URLField()
-    imageUrl = URLField()
-    newsSite = CharField(max_length=100)
-    featured = BooleanField(initial=False)
-    summary = CharField(max_length=300)
-    publishedAt = DateTimeField()
-    updatedAt = DateTimeField()
+    title = models.CharField(max_length=150)
+    url = models.URLField()
+    imageUrl = models.URLField()
+    newsSite = models.CharField(max_length=100)
+    featured = models.BooleanField(default=False)
+    summary = models.CharField(max_length=300)
+    publishedAt = models.DateTimeField()
+    updatedAt = models.DateTimeField()
     launches = models.ManyToManyField(Launch, blank=True)
     events = models.ManyToManyField(Event, blank=True)
