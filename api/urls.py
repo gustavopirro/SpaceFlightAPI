3d73bab13918ff270a89305aaa6c7af251447c40
gustavopirro <gustavo.pirro@gmail.com>
2022-01-28 09:45:29 -0300

    feat: implemented article, launch and event endpoints

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
-from api.views import hello_world
+from api.views import ArticleList, ArticleDetail
+from api.views import LaunchList, LaunchDetail
+from api.views import EventList, EventDetail
 
 
 urlpatterns = [
-    path('helloworld/', hello_world, name='hello_world')
+    path('articles/', ArticleList.as_view(), name='article_list'),
+    path('articles/<int:pk>', ArticleDetail.as_view(), name='article_detail'),
+    path('launches/', LaunchList.as_view(), name='launch_list'),
+    path('launches/<int:pk>', LaunchDetail.as_view(), name='launch_detail'),
+    path('events/', EventList.as_view(), name='event_list'),
+    path('events/<int:pk>', EventDetail.as_view(), name='event_detail'),
 ]
