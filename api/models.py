4adbb409fe8f894cb93d5f1fb8ae2eb9634bd076
gustavopirro <gustavo.pirro@gmail.com>
2022-01-28 08:39:49 -0300

    feat: implemented Article, Launch and Event models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.forms import BooleanField, CharField, DateTimeField, URLField
 
-# Create your models here.
+
+class Event(models.Model):
+    provider = CharField(max_length=100)
+
+
+class Launch(models.Model):
+    provider = CharField(max_length=100)
+
+
+class Article(models.Model):
+    title = CharField(max_length=150)
+    url = URLField()
+    imageUrl = URLField()
+    newsSite = CharField(max_length=100)
+    featured = BooleanField(initial=False)
+    summary = CharField(max_length=300)
+    publishedAt = DateTimeField()
+    updatedAt = DateTimeField()
+    launches = models.ManyToManyField(Launch, blank=True)
+    events = models.ManyToManyField(Event, blank=True)
