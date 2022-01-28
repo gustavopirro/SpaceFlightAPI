0a644cd497cfe734cffd1a322e658bf534d47435
gustavopirro <gustavo.pirro@gmail.com>
2022-01-28 09:29:54 -0300

    fix: removed unused code from settings.py

--- SpaceFlight/settings.py
+++ SpaceFlight/settings.py
@@ -20,11 +20,6 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-# dotenv_file = os.path.join(BASE_DIR, ".env")
-# if os.path.isfile(dotenv_file):
-#     dotenv.load_dotenv(dotenv_file)
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
