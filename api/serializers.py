8a2becad2b68dfdcf568c3aa394bce8e79be6f16
gustavopirro <gustavo.pirro@gmail.com>
2022-01-30 05:30:15 -0300

    feat: Implemented relationship serialization in Article model serializer class

--- api/serializers.py
+++ api/serializers.py
@@ -4,13 +4,6 @@ from api.models import Launch
 from api.models import Event
 
 
-class ArticleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Article
-        fields = '__all__'
-        depth = 1
-
-
 class LaunchSerializer(serializers.ModelSerializer):
     class Meta:
         model = Launch
@@ -21,3 +14,27 @@ class EventSerializer(serializers.ModelSerializer):
     class Meta:
         model = Event
         fields = '__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    launches = LaunchSerializer(many=True)
+    events = EventSerializer(many=True)
+
+    class Meta:
+        model = Article
+        fields = '__all__'
+        depth = 1
+
+    def create(self, validated_data):
+        events_data = validated_data.pop('events')
+        launchs_data = validated_data.pop('launches')
+        article = Article.objects.create(**validated_data)
+
+        for event_data in events_data:
+            new_event = Event.objects.create(article=article, **event_data)
+            article.events.add(new_event)
+        for launch_data in launchs_data:
+            new_launch = Launch.objects.create(article=article, **launch_data)
+            article.launches.add(new_launch)
+
+        return article
