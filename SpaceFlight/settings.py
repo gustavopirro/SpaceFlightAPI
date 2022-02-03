51ab4ef3bb5eea8355702c535bf87daf8e53a87c
gustavopirro <gustavo.pirro@gmail.com>
2022-02-03 08:18:25 -0300

    config: add postgresql connection config

--- SpaceFlight/settings.py
+++ SpaceFlight/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Flake Config
 # flake8: noqa
@@ -88,6 +89,8 @@ DATABASES = {
     }
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
